081c0530ceac8dd3ee691980716fb7f833e13d0b
Deniz <sea-97@hotmail.com>
2019-07-29 04:37:03 +0300

    add harmonical and geometrical mean tests

--- tests/StatTest.elm
+++ tests/StatTest.elm
@@ -1,16 +1,42 @@
-module StatTest exposing (powerfunctest)
+module StatTest exposing (powerfunctest,gmeantest,harmonictest)
 
 import Expect exposing (..)
 import Fuzz exposing (..)
-import Stat.Statistics exposing (powerfunc)
+import Test exposing (..)
+import Stat.Statistics exposing(powerfunc,geometricalmean,harmonicalmean)
+{--
+import Expect exposing (Expectation,Absolute)
+import Fuzz exposing (Fuzzer, int, list, string, float)
 import Test exposing (..)
 
+--}
 
 powerfunctest : Test
 powerfunctest =
     describe "power func test"
-        [ fuzz (tuple ( float, float )) "power func testing" <|
-            \( x, y ) ->
+         [ fuzz2 (floatRange 1.0 10.0) (floatRange 1.0 10.0)  "power func testing" <|
+            \x y ->
                 powerfunc x y
-                    |> Expect.within (Absolute 0.00000001) (x ^ y)
+                    |> Expect.within (Absolute 0.000000001) (x ^ y)
         ]
+
+gmeantest :Test
+gmeantest =
+     describe "geometric mean test"
+         [  test "geometrical mean func " <|
+              \x ->
+                 geometricalmean [18,2]
+                 |> Expect.equal 6
+
+         ]
+
+
+harmonictest :Test
+harmonictest =
+          describe "harmonical mean test"
+         [  test "harmonical mean  func" <|
+              \x ->
+                 harmonicalmean [1,2,3,4]
+                 |> Expect.within (Absolute 0.000000001)1.92
+
+         ]
